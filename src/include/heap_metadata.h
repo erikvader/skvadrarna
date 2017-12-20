41f0e43c04d0b944e69e2b89aec02e4849daf6ad
HampusAdolfsson <Hampus.Adolfsson.5594@student.uu.se>
2017-12-20 15:35:24 +0100

    Added documentation to heap_metadata.h

--- src/include/heap_metadata.h
+++ src/include/heap_metadata.h
@@ -1,66 +1,90 @@
 #ifndef __heap_metadata_h__
 #define __heap_metadata_h__
+/// \file heap_metadata.h
+/// \authors exp meta druid 200 LI+
 
 #include <stdlib.h>
 #include <stdbool.h>
 #include "gc.h"
 
 typedef int chunk_t;
-typedef bool bitarr_t; 
 
-// creates the metadata object
-// creates 3 different structs: heap_header, used_arr and free_pointers
-void hm_init(heap_t *heap,size_t size,bool unsafe_stack, float gc_threshold);
+/// The size of a heap header
+const size_t heap_header_size;
 
-// requests a place to store an object
-// moves a free pointer accordingly
-void *hm_get_free_space(heap_t *heap,size_t obj_siz);
+/// Initializes a header. The heap parameter is used to determine where to create the header;
+/// the header is placed right in front of the heap pointer. This means that there
+/// should be some free space before heap at least as big as heap_header_size.
+///
+/// \param heap the heap for which to allocate a header
+/// \param size the size of the heap, should be a multiple of 2048
+/// \param unsafe_stack true if pointers on the stack to this heap are to be considered unsafe pointers
+/// \param gc_threshold the memory pressure at which gc should be triggered for this heap
+void hm_init(heap_t *heap, size_t size, bool unsafe_stack, float gc_threshold);
 
-// gets an adress to store an object in a specific chunk
+/// Reserves a space on the heap where obj_size bytes can be written.
+/// Marks that space as occupied.
+///
+/// \param heap the heap for which to find the space
+/// \param obj_size the size of the space to reserve
+/// \return a pointer to the reserved space, or NULL if no such space was available
+void *hm_get_free_space(heap_t *heap, size_t obj_siz);
+
+/// Reserves a space in a specific chunk where obj_size bytes can be written.
+/// Marks that space as occupied.
+///
+/// \param heap the heap for which to find the space
+/// \param obj_size the size of the space to reserve
+/// \param index the chunk to reserve the memory in
+/// \return a pointer to the reserved space, or NULL if no such space was available
 void *hm_alloc_spec_chunk(heap_t *heap, size_t obj_siz, chunk_t index);
 
-// check if gc is currently over threshold.
+/// Checks if the percentage of allocated memory is above the threshold for garbage collection.
+///
+/// \param heap the heap to check
+/// \param return true if the memory pressure is above the threshold
 bool hm_over_threshold(heap_t *heap);
 
-// gets size avaiable in the heap
+/// Gets the total available space in a heap
+///
+/// \param heap the heap
+/// \return the available space in the heap
 size_t hm_size_available(heap_t *heap);
 
-// gets the used amount of memory in the heap
+/// Gets the total used space in a heap
+///
+/// \param heap the heap
+/// \return the used space in the heap
 size_t hm_size_used(heap_t *heap);
 
-// asks if a pointer is a pointer on the stack, and point to an object on it
+/// Checks if a pointer points to an active object on the heap.
+/// \param the heap
+/// \param pointer the pointer to check
+/// \param true if the pointer points to a living object on the heap
 bool hm_pointer_exists(heap_t *heap, void *pointer);
 
-// gets the amounts of chunk we have in the heap
+/// Gets the number of chunks on a heap
+/// \param heap the heap
+/// \return the number of chunks on the heap
 int hm_get_amount_chunks(heap_t *heap);
 
-// gets the chunk a pointer is pointing to
+/// Gets the chunk which contains the memory pointed to by pointer.
+///
+/// \param heap the heap
+/// \pointer pointer to the memory to locate
+/// \return if pointer points inside a chunk, gives that chunk, otherwise -1
 chunk_t hm_get_pointer_chunk(heap_t *heap, void *pointer);
 
-// resets a chunk back to 0, effectivly clearing it.
+/// Marks all memory ina chunk as unoccupied, making it available
+/// for future allocations.
 void hm_reset_chunk(heap_t *heap, chunk_t index);
 
 // returns a bool for every chunk, true if it is in use, false otherwise.
+/// Gives which chunks are used (have some occupied memory) and which are not.
+/// This data is written into data, which should be an array able to fit
+/// as many items as there are chunks in the heap, see \ref hm_get_amount_chunks
 void hm_get_used_chunks(heap_t *heap, bool *data);
 
-
-// will be hidden in the .c file.
-typedef struct heap_header{
-  // the start of where the free space is at init
-  void *heap_start;
-  // size of the usabe heap, that is, heap_size - header_size
-  size_t heap_siz;
-  // should always be 2048
-  size_t chunk_siz;
-  // if the stack is safe or not
-  bool unsafe_stack;
-  // the threashold for when a gc_event should trigger
-  float gc_threshold;
-  // the bit_array for which space is used
-  bitarr_t *used_arr;
-  // arr of pointers fore where the free space begins in every chunk
-  void **free_pointers;
-} heap_header_t;
 
 
 #endif
