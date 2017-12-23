e362a4cc9e72c9b40eafb8f0eec87d2fd8b927c2
Erik Rimskog <erik.rimskog@gmail.com>
2017-12-23 01:18:34 +0100

    revertade en commit som hamnade fel, sry
    This reverts commit 3532bd55537f41c68a752ae4657448111c462eea.

--- src/gc_event.c
+++
@@ -1,5 +0,0 @@
-#include "include/gc_event.h"
-
-void gce_run_gc_event(heap_t *heap){
-   
-}
--- src/include/gc_event.h
+++ src/include/gc_event.h
@@ -6,6 +6,6 @@
 //starts the gc_event.
 void gce_run_gc_event(heap_t *heap);
 
-// void gce_kor_kanske_en_gc_tack(heap_t *heap);
+void gce_kör_kanske_en_gc_tack(heap_t *heap);
 
 #endif
