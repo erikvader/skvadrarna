22785c3d31a046fe7c1e2f3b0cd298849d26f33d
Erik Rimskog <erik.rimskog@gmail.com>
2017-12-27 23:30:53 +0100

    finished main function

--- src/gc_event.c
+++ src/gc_event.c
@@ -1,4 +1,6 @@
 #include "include/gc_event.h"
+#include "include/heap_metadata.h"
+#include "include/stack_iter.h"
 
 // 1: hitta alla chunks som är används
 // 2: gå genom stacken och för varje valid pointer:
@@ -9,8 +11,38 @@
 //          gå till 2, men byt ut 'stacken' mot 'objektet'
 // 5: rensa alla chunks som användes innan samlingen (om de inte är unsafe)
 
+void explore(heap_t *heap, void* obj, bool unsafe, bool *unsafe_chunks){
+   
+}
 
 void gce_run_gc_event(heap_t *heap){
+
+   int num_chunks = hm_get_amount_chunks(heap);
    
+   bool unsafe = hm_is_unsafe(heap);
+   bool unsafe_chunks[num_chunks];
+
+   //get locked chunks
+   bool locked[num_chunks];
+   hm_get_used_chunks(heap, locked);
+
+   //loopa stack
+   void* start;
+   start = &start;
+
+   while(start != environ){
+      void **next = si_next_pointer(heap, &start);
+      if(*next != environ){
+         explore(heap, *next, unsafe, unsafe_chunks);
+      }
+   }
+
+   //free chunks
+   for(int i = 0; i < num_chunks; i++){
+      if(!unsafe_chunks[i] && locked[i]){
+         hm_reset_chunk(heap, i);
+      }
+   }
+
 }
 
