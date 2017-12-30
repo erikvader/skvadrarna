8d37506e1e25d964ea58b4a241b0d4d4863a3a55
Erik Rimskog <erik.rimskog@gmail.com>
2017-12-30 22:20:40 +0100

    removed helper comments

--- src/gc_event.c
+++ src/gc_event.c
@@ -4,16 +4,6 @@
 #include "include/object_metadata.h"
 #include <string.h>
 
-//kanske lite invalid
-// 1: hitta alla chunks som är används
-// 2: gå genom stacken och för varje valid pointer:
-//   3: markera chunken som unsafe (om vi kom från stacken)
-//   4: är det en forwarding-adress (objekt kopierat)?
-//     ja: ändra pointern till fwd-adress (om det inte är en unsafe stack-pointer)
-//     nej: kopiera objektet och sätt en fwd-adress (om vi inte är i en unsafe chunk)
-//          gå till 2, men byt ut 'stacken' mot 'objektet'
-// 5: rensa alla chunks som användes innan samlingen (om de inte är unsafe)
-
 // copy all live objects
 void explore(heap_t *heap, void **obj, bool *unsafe_chunks, bool *locked) {
 
