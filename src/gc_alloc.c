1965628ae251b50bf04191e29d956215d6b620eb
David Andersson <d.andersson.1994@gmail.com>
2018-01-02 13:52:57 +0100

    Fixat till gc_alloc.c
    Fixat till gc_alloc.c

--- src/gc_alloc.c
+++ src/gc_alloc.c
@@ -1,7 +1,6 @@
 #include "include/heap_metadata.h"
 #include "include/object_metadata.h"
-
-char *gca_str_copy(heap_t *heap, char *string);
+#include <string.h>
 
 void *gca_alloc_data(heap_t *heap, size_t bytes) {
     
@@ -11,78 +10,38 @@ void *gca_alloc_data(heap_t *heap, size_t bytes) {
       return NULL;
     }
     
-    om_build_pointerless(heap_pointer, bytes);
 
+    if (try == false) {
+        return NULL;        
+    }
+    
     return heap_pointer;
 }
 
 void *gca_alloc_struct(heap_t *heap, char *format)
 {
-    size_t bytes = om_get_size(format);
-
-    char *new_format = gca_alloc_data(heap, bytes);
-
-    new_format = gca_str_copy(heap, new_format); 
+    size_t bytes = om_size(format); //får storleken på strukten som format visar på
     
-    void* heap_pointer = hm_get_free_space(heap, bytes);
+    /* 
+     * Denna del görs även i om_build.
+     * 
+    
+    char *new_format = h_alloc_data(heap, bytes);
+
+    new_format = strcpy(new_format, format); 
+    */
+    
+    void* heap_pointer = hm_get_free_space(heap, bytes); //får tillräckligt stor pekare på heapen
 
     if (heap_pointer == NULL) {
       return NULL;
     }
     
-    om_build(heap_pointer, format);
+    bool try = om_build(heap, heap_pointer, format); //Bygger headern till objektet.
+    
+    if (try == false) {
+        return NULL;        
+    }
     
     return heap_pointer;
-    
 }
-
-/*
-size_t get_size(char *format) {
-    
-    size_t bytes = 0;
-    int counter = 0;
-    
-    while (format != '\0') 
-    {
-        if (format == '*')
-        {
-            bytes = bytes + sizeof(void*);
-            format++;
-            counter+;
-            
-        } else if (format == 'c')
-        {
-            bytes = bytes + sizeof(char);
-            format++;
-            counter+;
-            
-        } else if (format == 'i')
-        {
-            bytes = bytes + sizeof(int);
-            format++;
-            counter+;
-            
-        } else if (format == 'l')
-        {
-            bytes = bytes + sizeof(long);
-            format++;
-            counter+;
-          
-        } else if (format == 'f')
-        {
-            bytes = bytes + sizeof(float);
-            format++;
-            counter+;
-            
-        } else if (format == 'd')
-        {
-            bytes = bytes + sizeof(double);
-            format++;
-            counter+;
-        }
-        
-        format = format - counter;
-        return bytes;
-    }
-}
- */
