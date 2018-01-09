e7b8bad9b96c89108aee4e3d8bb0301e9ed47059
mallignos <mattias.aronssonadler.9978@student.uu.se>
2018-01-09 12:11:34 +0100

    snyggade till allt lite

--- src/gc.c
+++ src/gc.c
@@ -9,45 +9,45 @@
 
 
 heap_t *h_init(size_t bytes, bool unsafe_stack, float gc_threshold) {
-  void *mem_pos;
-  posix_memalign(&mem_pos,LARGE_ADDRESS,bytes);
-  heap_t *heap = mem_pos;
-  
-  hm_init(heap,bytes,unsafe_stack,gc_threshold);
+    void *mem_pos;
+    posix_memalign(&mem_pos, LARGE_ADDRESS, bytes);
+    heap_t *heap = mem_pos;
 
-  return heap;
+    hm_init(heap, bytes, unsafe_stack, gc_threshold);
+
+    return heap;
 }
 
 
 void h_delete(heap_t *heap) {
-  free(heap);
+    free(heap);
 }
 
 void h_delete_dbg(heap_t *heap, void *dbg_value) {
-  void *p = NULL;
-  void **iter = si_next_pointer(heap,&p);
-  while (iter != NULL) {
-    *iter = dbg_value;
-    iter = si_next_pointer(heap,iter);
-  }
-  
-  h_delete(heap);
+    void *p = NULL;
+    void **iter = si_next_pointer(heap, &p);
+    while(iter != NULL) {
+        *iter = dbg_value;
+        iter = si_next_pointer(heap, iter);
+    }
+
+    h_delete(heap);
 }
 
 void *h_alloc_struct(heap_t *heap, char *format) {
-  if (hm_over_threshold(heap)) {
-    h_gc(heap);
-  }
+    if(hm_over_threshold(heap)) {
+        h_gc(heap);
+    }
 
-  return gca_alloc_struct(heap,format);
+    return gca_alloc_struct(heap, format);
 }
 
 void *h_alloc_data(heap_t *heap, size_t bytes) {
-  if (hm_over_threshold(heap)) {
-    h_gc(heap);
-  }
-  
-  return gca_alloc_data(heap,bytes);
+    if(hm_over_threshold(heap)) {
+        h_gc(heap);
+    }
+
+    return gca_alloc_data(heap, bytes);
 }
 
 size_t h_gc(heap_t *heap) {
@@ -56,21 +56,21 @@ size_t h_gc(heap_t *heap) {
 
 
 size_t h_gc_dbg(heap_t *heap, bool unsafe_stack) {
-  size_t before = h_used(heap);
+    size_t before = h_used(heap);
 
-  //TODO: finns i erik-arbete
-  // gce_run_gc_event_dbg(heap, unsafe_stack);
+    //TODO: finns i erik-arbete
+    // gce_run_gc_event_dbg(heap, unsafe_stack);
 
-  size_t after = h_used(heap);
+    size_t after = h_used(heap);
 
-  return before - after;
+    return before - after;
 }
 
 size_t h_avail(heap_t *heap) {
-  return hm_size_available(heap);
+    return hm_size_available(heap);
 }
 
 size_t h_used(heap_t *heap) {
-  return hm_size_used(heap);
+    return hm_size_used(heap);
 }
 
